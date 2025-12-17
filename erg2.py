18994516f09b2dc67496903ef41df28957b46e95
Konstantinos Mousoulis <konmous04@gmail.com>
2025-12-17 20:54:24 +0200

    Second Commit

--- erg2.py
+++ erg2.py
@@ -1,7 +1,20 @@
 import torch
-from torch.utils.data import DataLoader
+import numpy as np
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
+from sklearn.decomposition import PCA
+
+def extract_classes(dataset, classes):
+    X = []
+    y = []
+    for img, label in dataset:
+        if label in classes:
+            img_flat = img.view(-1).numpy()
+            X.append(img_flat)
+            y.append(label)
+    X = np.array(X)
+    y = np.array(y)
+    return X, y
 
 transform = transforms.Compose([
     transforms.ToTensor(),
@@ -12,6 +25,12 @@ transform = transforms.Compose([
 train_dataset = datasets.CIFAR10(root="./data", train=True, download=True, transform=transform)
 test_dataset = datasets.CIFAR10(root="./data", train=False, download=True, transform=transform)
 
-batch_size = 128
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
-test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=4, pin_memory=True)
+classes_to_keep = [3, 5]
+X_train, y_train = extract_classes(train_dataset, classes_to_keep)
+X_test, y_test = extract_classes(test_dataset, classes_to_keep)
+
+pca_temp = PCA()
+pca_temp.fit(X_train)
+explained_variance = np.cumsum(pca_temp.explained_variance_ratio_)
+n_components = np.argmax(explained_variance >= 0.90) + 1
+print("Αριθμός συνιστωσών για 90% πληροφορίας:", n_components)
